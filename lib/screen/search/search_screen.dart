3519a2daa55d3d05fb1410171ca1a74f697c71ac
猫河 <catsriver@163.com>
2022-09-08 09:57:30 +0800

    refactor(search): 将AppBar拆分为Header widget

--- lib/screen/search/search_screen.dart
+++ lib/screen/search/search_screen.dart
@@ -1,62 +1,14 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import 'package:netease_cloud_music/res/gaps.dart';
 
-import '../../res/resources.dart';
+import 'widgets/header.dart';
 
 class SearchScreen extends StatelessWidget {
   const SearchScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        leading: IconButton(
-          icon: const FaIcon(FontAwesomeIcons.bars),
-          onPressed: () {},
-        ),
-        titleSpacing: 0,
-        title: Container(
-          height: 53.h,
-          decoration: BoxDecoration(
-            color: const Color(0xFFFDFDFD),
-            borderRadius: BorderRadius.circular(53.h),
-          ),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              FaIcon(
-                FontAwesomeIcons.magnifyingGlass,
-                size: 22.h,
-                color: Colours.fontColor3,
-              ),
-              Gaps.hGap7,
-              Text(
-                '天外来物',
-                style: Theme.of(context)
-                    .textTheme
-                    .headline6!
-                    .copyWith(color: Colours.fontColor3),
-              ),
-              Gaps.hGap7,
-              Text(
-                '薛之谦',
-                style: Theme.of(context)
-                    .textTheme
-                    .headline6!
-                    .copyWith(color: Colours.fontColor4),
-              ),
-            ],
-          ),
-        ),
-        actions: [
-          IconButton(
-            icon: const FaIcon(FontAwesomeIcons.microphoneLines),
-            onPressed: () {},
-          )
-        ],
-      ),
+    return const Scaffold(
+      appBar: Header(),
     );
   }
 }
---
+++ lib/screen/search/widgets/header.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+import '../../../res/resources.dart';
+
+class Header extends StatelessWidget implements PreferredSizeWidget {
+  const Header({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      leading: IconButton(
+        icon: const FaIcon(FontAwesomeIcons.bars),
+        onPressed: () {},
+      ),
+      titleSpacing: 0,
+      title: Container(
+        height: 53.h,
+        decoration: BoxDecoration(
+          color: const Color(0xFFFDFDFD),
+          borderRadius: BorderRadius.circular(53.h),
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            FaIcon(
+              FontAwesomeIcons.magnifyingGlass,
+              size: 22.h,
+              color: Colours.fontColor3,
+            ),
+            Gaps.hGap7,
+            Text(
+              '天外来物',
+              style: Theme.of(context)
+                  .textTheme
+                  .headline6!
+                  .copyWith(color: Colours.fontColor3),
+            ),
+            Gaps.hGap7,
+            Text(
+              '薛之谦',
+              style: Theme.of(context)
+                  .textTheme
+                  .headline6!
+                  .copyWith(color: Colours.fontColor4),
+            ),
+          ],
+        ),
+      ),
+      actions: [
+        IconButton(
+          icon: const FaIcon(FontAwesomeIcons.microphoneLines),
+          onPressed: () {},
+        )
+      ],
+    );
+  }
+
+  @override
+  Size get preferredSize => const Size.fromHeight(kToolbarHeight);
+}
