6ba4ed8805bcb880a5c3e43979c87bb1854282f2
猫河 <catsriver@163.com>
2022-09-08 12:02:32 +0800

    fix(search): 修复BannerItem组件拆分错误

--- lib/screen/search/search_screen.dart
+++ lib/screen/search/search_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+import '../../res/resources.dart';
 import 'widgets/banner_item.dart';
 import 'widgets/body.dart';
 import 'widgets/header.dart';
@@ -10,16 +12,25 @@ class SearchScreen extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return const Scaffold(
-      appBar: Header(),
+    return Scaffold(
+      appBar: const Header(),
       body: Body(
         children: [
-          // Banner
-          BannerItem(
-            imgUrl:
-                'http://p1.music.126.net/BcZXvoTKen7fPMDXezzScQ==/109951167853780577.jpg',
-            title: '广告',
-            titleColor: Colors.blue,
+          Container(
+            height: 190.h,
+            margin: EdgeInsets.symmetric(
+                horizontal: Dimens.hGapDp24, vertical: Dimens.vGapDp24 / 2),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(Dimens.radiusDp12),
+            ),
+            clipBehavior: Clip.antiAlias,
+            child: const BannerItem(
+              imgUrl:
+                  'http://p1.music.126.net/BcZXvoTKen7fPMDXezzScQ==/109951167853780577.jpg',
+              title: '广告',
+              titleColor: Colors.blue,
+            ),
           ),
         ],
       ),
--- lib/screen/search/widgets/banner_item.dart
+++ lib/screen/search/widgets/banner_item.dart
@@ -17,55 +17,45 @@ class BannerItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 190.h,
-      margin: EdgeInsets.symmetric(
-          horizontal: Dimens.hGapDp24, vertical: Dimens.vGapDp24 / 2),
-      decoration: BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.circular(Dimens.radiusDp12),
-      ),
-      clipBehavior: Clip.antiAlias,
-      child: Stack(
-        children: [
-          Image.network(
-            imgUrl,
-            fit: BoxFit.cover,
-            width: double.infinity,
-          ),
-          Positioned(
-            bottom: 0,
-            right: 0,
-            child: Container(
-              padding: EdgeInsets.symmetric(
-                horizontal: Dimens.hGapDp10,
-                vertical: Dimens.vGapDp6,
-              ),
-              decoration: ShapeDecoration(
-                color: titleColor,
-                shape: RoundedRectangleBorder(
-                  side: BorderSide(
-                    color: titleColor.withOpacity(.5),
-                    width: 1.w,
-                  ),
-                  borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(Dimens.radiusDp12),
-                    bottomRight: Radius.circular(Dimens.radiusDp12),
-                  ),
+    return Stack(
+      children: [
+        Image.network(
+          imgUrl,
+          fit: BoxFit.cover,
+          width: double.infinity,
+        ),
+        Positioned(
+          bottom: 0,
+          right: 0,
+          child: Container(
+            padding: EdgeInsets.symmetric(
+              horizontal: Dimens.hGapDp10,
+              vertical: Dimens.vGapDp6,
+            ),
+            decoration: ShapeDecoration(
+              color: titleColor,
+              shape: RoundedRectangleBorder(
+                side: BorderSide(
+                  color: titleColor.withOpacity(.5),
+                  width: 1.w,
                 ),
-              ),
-              child: Text(
-                title,
-                style: TextStyle(
-                  fontSize: Dimens.fontSp16,
-                  color: Colours.fontColorWihite,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(Dimens.radiusDp12),
+                  bottomRight: Radius.circular(Dimens.radiusDp12),
                 ),
-                textAlign: TextAlign.center,
               ),
             ),
+            child: Text(
+              title,
+              style: TextStyle(
+                fontSize: Dimens.fontSp16,
+                color: Colours.fontColorWihite,
+              ),
+              textAlign: TextAlign.center,
+            ),
           ),
-        ],
-      ),
+        ),
+      ],
     );
   }
 }
