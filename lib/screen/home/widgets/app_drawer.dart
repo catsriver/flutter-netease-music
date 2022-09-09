edf90c0e20817ae9464e5cd2a56e86ac159dac6e
猫河 <catsriver@163.com>
2022-09-09 09:34:20 +0800

    feat(drawer): 添加用户资料、扫码按钮

--- lib/screen/home/widgets/app_drawer.dart
+++ lib/screen/home/widgets/app_drawer.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:netease_cloud_music/widgets/svg_icon.dart';
 
 import '../../../res/resources.dart';
 
@@ -19,7 +20,61 @@ class AppDrawer extends StatelessWidget {
           children: [
             AppBar(
               automaticallyImplyLeading: false,
-              backgroundColor: Colors.pink,
+              backgroundColor: Colours.secondaryBgColor,
+              titleSpacing: Dimens.hGapDp24,
+              title: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  // 左侧头像信息
+                  GestureDetector(
+                    onTap: () {
+                      print('点击了用户头像');
+                    },
+                    child: Row(
+                      children: [
+                        // 头像
+                        SizedBox(
+                          width: 40.w,
+                          height: 40.w,
+                          child: const CircleAvatar(
+                            backgroundImage: NetworkImage(
+                                'https://c-ssl.dtstatic.com/uploads/item/201404/02/20140402105931_wtee5.thumb.400_0.jpeg'),
+                          ),
+                        ),
+
+                        // 间距
+                        Gaps.hGap15,
+
+                        // 用户名
+                        Text(
+                          '立即登陆',
+                          style: TextStyle(
+                            fontSize: Dimens.fontSp22,
+                            color: Colours.fontColor1,
+                          ),
+                        ),
+
+                        // 箭头
+                        SvgIcon(
+                          SvgIcons.chevronRight,
+                          size: 28.h,
+                        ),
+                      ],
+                    ),
+                  ),
+
+                  // 右侧扫码图标
+                  GestureDetector(
+                    onTap: () {
+                      print('点击了扫一扫图标');
+                    },
+                    child: SvgIcon(
+                      SvgIcons.scan,
+                      size: 40.w,
+                    ),
+                  ),
+                ],
+              ),
             ),
           ],
         ),
