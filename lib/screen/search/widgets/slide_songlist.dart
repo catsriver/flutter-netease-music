38d68a25dc086c9ef7e9a78a547d53e968d2671d
猫河 <catsriver@163.com>
2022-09-17 15:15:24 +0800

    feat: 点击歌曲菜单时跳转到歌曲播放页面

--- lib/screen/search/widgets/slide_songlist.dart
+++ lib/screen/search/widgets/slide_songlist.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:go_router/go_router.dart';
 
 import '../../../res/resources.dart';
+import '../../play_music/play_music_screen.dart';
 
 class SlideSongList extends StatelessWidget {
   const SlideSongList({
@@ -24,135 +26,139 @@ class SlideSongList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: 90.h,
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          // 左侧图片
-          Container(
-            width: 75.w,
-            height: 75.h,
-            clipBehavior: Clip.antiAlias,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(Dimens.radiusDp12),
+    return GestureDetector(
+      onTap: () => GoRouter.of(context).go(PlayMusicScreen.routeName),
+      child: SizedBox(
+        height: 90.h,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            // 左侧图片
+            Container(
+              width: 75.w,
+              height: 75.h,
+              clipBehavior: Clip.antiAlias,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(Dimens.radiusDp12),
+              ),
+              child: Image.network(
+                imageUrl,
+              ),
             ),
-            child: Image.network(
-              imageUrl,
-            ),
-          ),
 
-          // 间距
-          Gaps.hGap15,
+            // 间距
+            Gaps.hGap15,
 
-          // 右侧列表项
-          Container(
-            width: 378.w,
-            decoration: BoxDecoration(
-              border: bottom
-                  ? Border(
-                      bottom: BorderSide(
-                        color: const Color(0xFFF3F3F3),
-                        width: 1.h,
-                      ),
-                    )
-                  : null,
-            ),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                // 左侧歌曲详情
-                Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Row(
-                      children: [
-                        // 歌曲名称
-                        Text(
-                          title,
-                          style: TextStyle(
-                            fontSize: Dimens.fontSp22,
-                            color: Colours.fontColor1,
-                          ),
+            // 右侧列表项
+            Container(
+              width: 378.w,
+              decoration: BoxDecoration(
+                border: bottom
+                    ? Border(
+                        bottom: BorderSide(
+                          color: const Color(0xFFF3F3F3),
+                          width: 1.h,
                         ),
-
-                        // 间距
-                        Gaps.hGap7,
-
-                        Text(
-                          '-',
-                          style: TextStyle(
-                            fontSize: Dimens.fontSp16,
-                            color: Colours.fontColor4,
-                          ),
-                        ),
-
-                        Gaps.hGap5,
-
-                        // 歌手
-                        for (int i = 0; i < artists.length; i++)
+                      )
+                    : null,
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  // 左侧歌曲详情
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Row(
+                        children: [
+                          // 歌曲名称
                           Text(
-                            i > 0 ? '/${artists[i]}' : artists[i],
+                            title,
+                            style: TextStyle(
+                              fontSize: Dimens.fontSp22,
+                              color: Colours.fontColor1,
+                            ),
+                          ),
+
+                          // 间距
+                          Gaps.hGap7,
+
+                          Text(
+                            '-',
                             style: TextStyle(
                               fontSize: Dimens.fontSp16,
                               color: Colours.fontColor4,
                             ),
-                          )
-                      ],
-                    ),
+                          ),
 
-                    Gaps.vGap6,
+                          Gaps.hGap5,
 
-                    // subTitle
-                    if (subTitleType == 'songRcmdTag')
-                      Container(
-                        padding: EdgeInsets.symmetric(
-                          horizontal: Dimens.hGapDp5,
-                          vertical: Dimens.vGapDp4,
-                        ),
-                        decoration: BoxDecoration(
-                          color: const Color(0xFFFE7E3A).withOpacity(.5),
-                          borderRadius: BorderRadius.circular(Dimens.radiusDp4),
-                        ),
-                        child: Text(
-                          subTitle,
-                          style: TextStyle(
-                            fontSize: Dimens.fontSp14,
-                            color: const Color(0xFFFE7E3A),
+                          // 歌手
+                          for (int i = 0; i < artists.length; i++)
+                            Text(
+                              i > 0 ? '/${artists[i]}' : artists[i],
+                              style: TextStyle(
+                                fontSize: Dimens.fontSp16,
+                                color: Colours.fontColor4,
+                              ),
+                            )
+                        ],
+                      ),
+
+                      Gaps.vGap6,
+
+                      // subTitle
+                      if (subTitleType == 'songRcmdTag')
+                        Container(
+                          padding: EdgeInsets.symmetric(
+                            horizontal: Dimens.hGapDp5,
+                            vertical: Dimens.vGapDp4,
+                          ),
+                          decoration: BoxDecoration(
+                            color: const Color(0xFFFE7E3A).withOpacity(.5),
+                            borderRadius:
+                                BorderRadius.circular(Dimens.radiusDp4),
+                          ),
+                          child: Text(
+                            subTitle,
+                            style: TextStyle(
+                              fontSize: Dimens.fontSp14,
+                              color: const Color(0xFFFE7E3A),
+                            ),
                           ),
                         ),
-                      ),
-                  ],
-                ),
-
-                // 右侧播放按钮
-                Container(
-                  padding: EdgeInsets.fromLTRB(
-                    Dimens.hGapDp10,
-                    Dimens.vGapDp6,
-                    Dimens.hGapDp7,
-                    Dimens.vGapDp6,
+                    ],
                   ),
-                  decoration: ShapeDecoration(
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(Dimens.radiusDp12),
-                      side: BorderSide(
-                        width: 2.w,
-                        color: Colours.fontColor4,
+
+                  // 右侧播放按钮
+                  Container(
+                    padding: EdgeInsets.fromLTRB(
+                      Dimens.hGapDp10,
+                      Dimens.vGapDp6,
+                      Dimens.hGapDp7,
+                      Dimens.vGapDp6,
+                    ),
+                    decoration: ShapeDecoration(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(Dimens.radiusDp12),
+                        side: BorderSide(
+                          width: 2.w,
+                          color: Colours.fontColor4,
+                        ),
                       ),
                     ),
+                    child: FaIcon(
+                      FontAwesomeIcons.play,
+                      color: Colours.fontColor4,
+                      size: 20.h,
+                    ),
                   ),
-                  child: FaIcon(
-                    FontAwesomeIcons.play,
-                    color: Colours.fontColor4,
-                    size: 20.h,
-                  ),
-                ),
-              ],
+                ],
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
